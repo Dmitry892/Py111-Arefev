ae417914f153d239048eaf7a820b91cc4d71f5df
Dmitry Arefev <arefev.dmitry@gmail.com>
2021-01-07 17:54:01 +0300

    my fact Арефьев Дмитрий

--- Tasks/c1_fact.py
+++ Tasks/c1_fact.py
@@ -1,11 +1,20 @@
+# Арефьев Дмитрий
+
 def factorial_recursive(n: int) -> int:
     """
     Calculate factorial of number n (> 0) in recursive way
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    try:
+        if n < 0:
+            raise ValueError
+        elif n == 0:
+            return 1
+        else:
+            return factorial_recursive(n - 1) * n
+    except ValueError:
+        raise
 
 
 def factorial_iterative(n: int) -> int:
@@ -15,5 +24,15 @@ def factorial_iterative(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    try:
+        if n < 0:
+            raise ValueError
+        elif n == 0:
+            return 1
+        else:
+            factorial = 1
+            for f in range(n, 0, -1):
+                factorial *= f
+            return factorial
+    except ValueError:
+        raise
