008f28f967d4411b0d48ae5e706f94834aee805a
Dmitry Arefev <arefev.dmitry@gmail.com>
2021-01-09 18:15:38 +0300

    my Taylor Арефьев Дмитрий

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -4,6 +4,13 @@ Taylor series
 from typing import Union
 
 
+def factorial(fac: int) -> int:
+    factorial_ = 1
+    for f in range(fac, 0, -1):
+        factorial_ *= f
+    return factorial_
+
+
 def ex(x: Union[int, float]) -> float:
     """
     Calculate value of e^x with Taylor series
@@ -11,8 +18,14 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    delta = 0.0001
+    exp = 0
+    for n in range(10000000):
+        value = x ** n / factorial(n)
+        if value <= delta:
+            return exp
+        else:
+            exp += value
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -22,5 +35,11 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    delta = 0.0001
+    current_value = 0
+    for n in range(10000000):
+        current_value += (-1) ** n * (x ** (2 * n + 1)) / factorial(2 * n + 1)
+        next_value = (-1) ** (n + 1) * (x ** (2 * (n + 1) + 1)) / factorial(2 * (n + 1) + 1)
+        next_value += current_value
+        if abs(current_value - next_value) <= delta:
+            return current_value
