92fcbd908526647a009ef6db3f7e06ac78c8df8c
Dmitry Arefev <arefev.dmitry@gmail.com>
2021-01-19 00:53:59 +0300

    my bubble_sort Арефьев Дмитрий

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -8,4 +8,8 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    for i in range(len(container)):
+        for j in range(len(container) - 1):
+            if container[i] < container[j]:
+                container[i], container[j] = container[j], container[i]
     return container
